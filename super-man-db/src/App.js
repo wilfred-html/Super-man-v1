78a3de2773e8aa8d6d1b251401f2101e2fbbf438
wilfred-html <wolfdog.singini@gmail.com>
2025-04-11 21:24:27 +0200

    v1

---
+++ super-man-db/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SuperManDB from './SuperManDB';
+
+function App() {
+  return (
+    <div className="App">
+      <SuperManDB />
+    </div>
+  );
+}
+
+export default App;
